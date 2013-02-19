3044688ae5eb76d3641a2d53dd51b40be9afd25b
Ivar Prudnikov <aprudnikovas@yahoo.com>
2013-02-19 23:37:40 +0000

    added video page

--- routes/index.js
+++ routes/index.js
@@ -24,6 +24,28 @@
 
         });
 
+		app.get('/setKey', function (req, res) {
+
+			app.set('youtubeKey',req.query.id)
+
+			var data = {
+				title : 'Youtube hoover'
+				, query : req.query
+			};
+
+			console.log( '>>> key : ' + app.get('youtubeKey') )
+
+			return res.format({
+				html: function(){
+					res.render('index', data);
+				},
+				json: function(){
+					res.send(data);
+				}
+			});
+
+		});
+
 		app.get('/search', function (req, res) {
 
 			var data = {
@@ -47,11 +69,34 @@
 
 			}
 
-			if(req.query.q && req.query.key){
-				youtube.search(req.query.q, req.query.key, show);
-			} else {
-				show(data.results)
+			youtube.search(app.get('youtubeKey'),req.query.q, show);
+
+		});
+
+		app.get('/video/:id', function (req, res) {
+
+			var data = {
+				title : 'Youtube hoover'
+				, query : req.query
+				, results : {}
+			};
+
+			function show(response){
+
+				data.results = response;
+
+				return res.format({
+					html: function(){
+						res.render('video', data);
+					},
+					json: function(){
+						res.send(data);
+					}
+				});
+
 			}
+
+			youtube.findVideo(app.get('youtubeKey'),req.params.id,show);
 
 		});
 
--- services/youtube.js
+++ services/youtube.js
@@ -2,22 +2,32 @@
 
 var agent = require('superagent');
 
-exports.search = function(){
+exports.search = function(key,q,cb){
 
 	var youtube_search = 'https://www.googleapis.com/youtube/v3/search'
-		, l = arguments.length
-		, cb = arguments[l-1]
 		, params = {
 			part : 'snippet'
 			, maxResults : 50
+			, q : q
+			, key : key
 		};
 
-	if(l > 2){
-		params['q'] = arguments[0];
-		params['key'] = arguments[1];
-	}
-
 	agent.get( youtube_search ).query( params ).end(function(res){
+		return cb(res.body);
+	});
+
+}
+
+exports.findVideo = function(key,id,cb){
+
+	var youtube_video = 'https://www.googleapis.com/youtube/v3/videos'
+		, params = {
+			part : 'id,snippet,contentDetails,player,statistics,status,topicDetails'
+			, id : id
+			, key : key
+		};
+
+	agent.get( youtube_video ).query( params ).end(function(res){
 		return cb(res.body);
 	});
 
